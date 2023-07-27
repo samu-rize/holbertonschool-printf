968fd974655f88a2b38681554ab7803beb562210
samu-rize <6710@holbertonstudents.com>
2023-07-27 18:15:41 +0100

    fix erros

--- _printf.c
+++ _printf.c
@@ -28,6 +28,10 @@ int _printf(const char *format, ...)
 	};
 
 	va_start(arg, format);
+
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+
 	while (format != NULL && format[i] != '\0')
 	{
 		if (format[i] == '%')

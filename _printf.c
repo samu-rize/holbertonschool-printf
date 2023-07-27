7bc0e6c32cdb342ebfbf8bfb43f863944d12bd25
RanimZaouga <ranim.zaougaa@gmail.com>
2023-07-27 15:36:23 +0100

    create _printf function

---
+++ _printf.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * _printf - Custom implementation of the printf function.
+ * @format: The format string containing format specifiers.
+ * @...: Additional arguments depending on format specifiers.
+ *
+ * Description: This function is a custom implementation of the printf function
+ *              that supports the following format specifiers: '%c' for characters,
+ *              '%s' for strings, and '%%' to print a percent symbol.
+ *              It prints the formatted output to the standard output.
+ *
+ * Return: The total number of characters printed (excluding the null byte
+ *         used to end output to strings) or -1 if an error occurs.
+ */
+
+int _printf(const char *format, ...)
+{
+	int i = 0, j, n = 0;
+
+	va_list arg;
+
+	va_start(arg, format)
+	while (format != NULL && format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			j = 0;
+			i++;
+			while (choice[j].letter != '\0')
+			{
+				if (choice[j].letter == format[i])
+				{
+					n += choice[j].func(arg);
+					i++;
+				}
+				j++;
+			}
+		}
+		_putchar(format[i]);
+		n++;
+	}
+	va_end(arg);
+	_putchar('\n');
+	return (n);
+}

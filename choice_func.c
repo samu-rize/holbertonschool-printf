2dc8d8550c10846f437c19100e07c031adf4c27b
samu-rize <6710@holbertonstudents.com>
2023-07-27 18:13:24 +0100

    fix erros

--- choice_func.c
+++ choice_func.c
@@ -54,9 +54,8 @@ int print_string(va_list arg)
  * Return: The number of characters printed (always 1).
  */
 
-int print_percent(va_list arg)
+int print_percent(__attribute__((unused))va_list arg)
 {
-	(void)arg;
 	_putchar('%');
 	return (1);
 }

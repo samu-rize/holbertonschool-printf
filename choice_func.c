dbd18f26fc6d3077d24f8874ffc8eefcaa2daf73
samu-rize <6710@holbertonstudents.com>
2023-07-28 11:02:20 +0100

    fix betty errors

--- choice_func.c
+++ choice_func.c
@@ -38,3 +38,34 @@ int print_percent(__attribute__((unused))va_list list)
 	_write_char('%');
 	return (1);
 }
+
+/**
+ * print_decimal - Prints a percent symbol
+ * @list: list of arguments
+ * Return: Will return the amount of characters printed.
+ */
+int print_decimal(va_arg list)
+{
+	int d = 1, len = 0, number = va_arg(list, int);
+	unsigned int tmp;
+
+	tmp = number;
+	if (number < 0)
+	{
+		_write_char('-');
+		tmp = -number;
+		len++;
+	}
+	while (tmp / d > 9)
+	{
+		d *= 10;
+	}
+	while (d != 0)
+	{
+		_write_char(tmp / d + '0');
+		tmp %= d;
+		len++;
+		d /= 10;
+	}
+	return (len);
+}
